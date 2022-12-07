c96aa21744f16089d4cc864834b555d0697f3c2a
Martin McWhorter <martin.mcwhorter@verizonconnect.com>
2022-12-07 11:17:28 +0000

    update message

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let stream = include_str!("../input.txt").to_string();
 
     println!("start of packet {}", find_start_of_segment(4, &stream));
-    println!("start of packet {}", find_start_of_segment(14, &stream));
+    println!("start of message {}", find_start_of_segment(14, &stream));
 }
 
 fn find_start_of_segment(size: usize, stream: &String) -> usize {
